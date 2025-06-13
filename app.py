bd19407f6a49f50e78619ae7ecc8c6a9a91fa0c9
solganeo <49591702+solganeo@users.noreply.github.com>
2025-06-14 01:05:54 +0200

    Premier commit : initialisation du projet

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__, static_folder='static', static_url_path='/static')
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # port imposé par Heroku
+    app.run(host="0.0.0.0", port=port)
